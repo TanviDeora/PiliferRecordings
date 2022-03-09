4985130a9801f4b7f7e49d026a91fa3a28a3e78b
Tanvi Deora <tanvid2@uw.edu>
2022-03-09 09:52:09 -0800

    user input and motor amplitude check
    Added user input to get filename and directory. Also added an if statement to make sure the motor input in within +-5V

--- MatlabCommandCodes/RunExperiment.m
+++ MatlabCommandCodes/RunExperiment.m
@@ -1,13 +1,20 @@
 %%
 clear all
 clc
-
 %% setting up daq
 nidaq = nidaqSetup();
 
+%% get filename
+name = input('Input Filename:','s');
+date = datestr(now,'yyyy-mm-dd_HH-MM');
+filename = strcat(name,'_',date,'.mat');
+
+% select directory
+selpath = uigetdir('../PiliferRecordings/','Select the directory to save file');
+
 %% Generating WhiteNoise
 Fc = 300;
-amp = 2;
+amp = 4;
 T = 10;
 repeatNoise = 1;
 numRepeat = 30;
@@ -20,13 +27,17 @@ numRepeat = 30;
 % cycles = 10; 
 % stepsHz = 10;
 % Flow = 1;
-% 
+% % generate your stimulus pattern
 % [daqoutput, StimulusVariables] = generateSequenceOfSines(Fhigh, amp, cycles, stepsHz, nidaq, Flow);
+%% ensure motor output is less than 5V
 
-%% sending data to motor
-% write into the daq
-inScanData = readwrite(nidaq,daqoutput,"OutputFormat","Matrix");
-
-%% saving your data
-% filename =  'm17_sineSeq_1.mat';
-% save(filename, 'StimulusVariables', 'inScanData')
+if max(daqoutput(:,1)) >= 5
+    disp 'motor amplitude is set to equal or higher than 5. The code will exit without running motor!'
+else
+    % sending data to motor
+    % read and write into the daq
+    inScanData = readwrite(nidaq,daqoutput,"OutputFormat","Matrix");
+    
+    % saving your data
+    save(strcat(selpath,'/', filename), 'StimulusVariables', 'inScanData')
+end
